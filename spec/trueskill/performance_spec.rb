96ed46ca94bd6fcab40a214432c7c22d7e4f9d14
Cody Maggard <codym@everfi.com>
2012-02-17 13:57:13 -0500

    No more error on data not being passed in

--- spec/trueskill/performance_spec.rb
+++ spec/trueskill/performance_spec.rb
@@ -2,21 +2,12 @@ require 'spec_helper'
 
 describe "Trueskill::Performance" do
   let(:performance) do
-    Trueskill::Performance.new(player: "a",
+    @player = mock(Trueskill::Player)
+    Trueskill::Performance.new(player: @player,
                                score: 25)
   end
-  it "should require a player" do
-    lambda { 
-      perf = Trueskill::Performance.new(score: 25)
-    }.should raise_error(ArgumentError)
-  end
-  it "should require a score" do
-    lambda {
-      perf = Trueskill::Performance.new(player: "a")
-    }.should raise_error(ArgumentError)
-  end
   it "should let you get the player" do
-    performance.player.should == "a"
+    performance.player.should == @player
   end
   it "should let you get the score" do
     performance.score.should == 25
