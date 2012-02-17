da0a2b0783a8b0df2843c69f10fd773cd3d9fd3f
Cody Maggard <codym@everfi.com>
2012-02-17 12:25:36 -0500

    Goodbye, FactoryGirl

--- spec/factories.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-  sequence(:player_id) { |n| "player#{n}" }
-
-  factory :player, :class => Trueskill::Player do
-    id { FactoryGirl.generate :player_id }
-    mu 25.0
-    sigma 25.0 / 3.0
-  end
-end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,12 +2,11 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
 require 'trueskill'
-#require 'factory_girl'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 RSpec.configure do |config|
-  #FactoryGirl.find_definitions
+
 end
